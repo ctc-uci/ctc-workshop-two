583fb3518bd1b7a02ed549f39a5d78f97bcb7249
Ryan Su <ryan.parkcity@gmail.com>
2024-11-10 14:28:37 -0800

    Finished Theory Two

--- routes/theoryTwoRouter.js
+++ routes/theoryTwoRouter.js
@@ -9,10 +9,33 @@ require('dotenv').config();
 theoryTwoRouter.use(express.json());
 
 // Notice how we're using a get (But there's also others like post, put/patch, delete)
-theoryTwoRouter.get('/', async (req, res) => {
+theoryTwoRouter.get('/easy', async (req, res) => {
   try {
-    const events = await pool.query(`SELECT * from theories 
-        WHERE title = 'Kevin has a crippling league addiction';`);
+    const events = await pool.query(`SELECT * from evidence WHERE theory_id = 2;`);
+    res.status(200).json(events.rows);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+theoryTwoRouter.get('/medium', async (req, res) => {
+  try {
+    const events = await pool.query(`SELECT COUNT(id) from evidence WHERE theory_id = 2;`);
+    res.status(200).json(events.rows);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+theoryTwoRouter.get('/hard', async (req, res) => {
+  try {
+    const events = await pool.query(
+      `SELECT theories.title FROM theories
+      JOIN believability ON theories.id = believability.theory_id
+      GROUP BY theories.id, theories.title
+      ORDER BY AVG(believability.believability_score)
+      ASC LIMIT 1`,
+    );
     res.status(200).json(events.rows);
   } catch (err) {
     res.status(500).json(err.message);
